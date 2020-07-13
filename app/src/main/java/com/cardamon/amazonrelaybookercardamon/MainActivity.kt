04e680b9afe779c7603e319bef3d1d03afd22cb2
dmitriikargashin <dmitriy.kargashin@gmail.com>
2020-07-13 09:45:55 -0400

    Bottom menu ready

--- app/src/main/java/com/cardamon/amazonrelaybookercardamon/MainActivity.kt
+++ app/src/main/java/com/cardamon/amazonrelaybookercardamon/MainActivity.kt
@@ -13,7 +13,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-      //  val navView: BottomNavigationView = findViewById(R.id.nav_view)
+        val navView: BottomNavigationView = findViewById(R.id.nav_view)
 
         val navController = findNavController(R.id.nav_host_fragment)
         // Passing each menu ID as a set of Ids because each
@@ -21,6 +21,6 @@ class MainActivity : AppCompatActivity() {
         val appBarConfiguration = AppBarConfiguration(setOf(
                 R.id.navigation_home, R.id.navigation_notifications))
         setupActionBarWithNavController(navController, appBarConfiguration)
-      //  navView.setupWithNavController(navController)
+        navView.setupWithNavController(navController)
     }
 }
